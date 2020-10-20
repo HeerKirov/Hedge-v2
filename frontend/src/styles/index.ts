8be25cc8cc8c9a3af9a4d62203b318416f07cd14
Heer Kirov <HeerKirov@outlook.com>
2020-10-21 00:43:29 +0800

    补充更新样式文件

--- frontend/src/styles/index.ts
+++ frontend/src/styles/index.ts
@@ -1,6 +1,5 @@
-// import 'bulma/css/bulma.min.css'
-import './bulma.scss'
 import '@fortawesome/fontawesome-free/css/all.min.css'
+import './bulma.scss'
 import './format.scss'
 import './layout.scss'
 import './display.scss'
