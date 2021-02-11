0717e8eb9254b2a67caf918819b0ceafd2a6ff0b
Heer Kirov <HeerKirov@outlook.com>
2021-02-12 04:43:22 +0800

    查询管理器: 引入执行计划整体缓存和options变更响应

--- server/src/main/kotlin/com/heerkirov/hedge/server/components/manager/query/QueryManager.kt
+++ server/src/main/kotlin/com/heerkirov/hedge/server/components/manager/query/QueryManager.kt
@@ -13,45 +13,84 @@ import com.heerkirov.hedge.server.library.compiler.translator.*
 import com.heerkirov.hedge.server.library.compiler.translator.visual.*
 import com.heerkirov.hedge.server.library.compiler.utils.AnalysisResult
 import com.heerkirov.hedge.server.library.compiler.utils.CompileError
+import java.util.concurrent.ConcurrentHashMap
 
 class QueryManager(private val data: DataRepository) {
     private val queryer = MetaQueryer(data)
     private val options = OptionsImpl()
 
-    fun querySchema(text: String, dialect: Dialect): AnalysisResult<VisualQueryPlan, CompileError<*>> {
-        val lexicalResult = LexicalAnalyzer.parse(text, options)
-        if(lexicalResult.result == null) {
-            return AnalysisResult(null, warnings = lexicalResult.warnings, errors = lexicalResult.errors)
-        }
-        val grammarResult = GrammarAnalyzer.parse(lexicalResult.result)
-        if(grammarResult.result == null) {
-            return AnalysisResult(null, warnings = grammarResult.warnings, errors = grammarResult.errors)
-        }
-        val semanticResult = SemanticAnalyzer.parse(grammarResult.result, when (dialect) {
-            Dialect.ILLUST -> IllustDialect::class
-            Dialect.ALBUM -> AlbumDialect::class
-            Dialect.AUTHOR_AND_TOPIC -> AuthorAndTopicDialect::class
-            Dialect.ANNOTATION -> AnnotationDialect::class
-        })
-        if(semanticResult.result == null) {
-            return AnalysisResult(null, warnings = semanticResult.warnings, errors = semanticResult.errors)
-        }
-        val translatorResult = Translator.parse(semanticResult.result, queryer, options)
-        if(translatorResult.result == null) {
-            return AnalysisResult(null, warnings = translatorResult.warnings, errors = translatorResult.errors)
-        }
+    private val executePlanCache = ConcurrentHashMap<DialectAndText, AnalysisResult<VisualQueryPlan, CompileError<*>>>()
 
-        return AnalysisResult(translatorResult.result, warnings = lexicalResult.warnings + grammarResult.warnings + semanticResult.warnings + translatorResult.warnings)
+    fun querySchema(text: String, dialect: Dialect): AnalysisResult<VisualQueryPlan, CompileError<*>> {
+        return executePlanCache.computeIfAbsent(DialectAndText(dialect, text)) { key ->
+            val lexicalResult = LexicalAnalyzer.parse(key.text, options)
+            if(lexicalResult.result == null) {
+                return@computeIfAbsent AnalysisResult(null, warnings = lexicalResult.warnings, errors = lexicalResult.errors)
+            }
+            val grammarResult = GrammarAnalyzer.parse(lexicalResult.result)
+            if(grammarResult.result == null) {
+                return@computeIfAbsent AnalysisResult(null, warnings = lexicalResult.warnings + grammarResult.warnings, errors = grammarResult.errors)
+            }
+            val semanticResult = SemanticAnalyzer.parse(grammarResult.result, when (key.dialect) {
+                Dialect.ILLUST -> IllustDialect::class
+                Dialect.ALBUM -> AlbumDialect::class
+                Dialect.AUTHOR_AND_TOPIC -> AuthorAndTopicDialect::class
+                Dialect.ANNOTATION -> AnnotationDialect::class
+            })
+            if(semanticResult.result == null) {
+                return@computeIfAbsent AnalysisResult(null, warnings = unionList(lexicalResult.warnings, grammarResult.warnings, semanticResult.warnings), errors = grammarResult.errors)
+            }
+            val translatorResult = Translator.parse(semanticResult.result, queryer, options)
+
+            AnalysisResult(translatorResult.result, warnings = unionList(lexicalResult.warnings, grammarResult.warnings, semanticResult.warnings, translatorResult.warnings), errors = translatorResult.errors)
+        }
+    }
+
+    private fun <T> unionList(vararg list: List<T>): List<T> {
+        val result = ArrayList<T>(list.sumBy { it.size })
+        for (i in list) {
+            result.addAll(i)
+        }
+        return result
     }
 
     enum class Dialect { ILLUST, ALBUM, AUTHOR_AND_TOPIC, ANNOTATION }
 
-    private inner class OptionsImpl : LexicalOptions, TranslatorOptions {
-        private val queryOptions by lazy { data.metadata.query }
+    private data class DialectAndText(val dialect: Dialect, val text: String)
 
-        override val translateUnderscoreToSpace: Boolean get() = queryOptions.translateUnderscoreToSpace
-        override val chineseSymbolReflect: Boolean get() = queryOptions.chineseSymbolReflect
-        override val warningLimitOfUnionItems: Int get() = queryOptions.warningLimitOfUnionItems
-        override val warningLimitOfIntersectItems: Int get() = queryOptions.warningLimitOfIntersectItems
+    private inner class OptionsImpl : LexicalOptions, TranslatorOptions {
+        private var _translateUnderscoreToSpace: Boolean? = null
+        private var _chineseSymbolReflect: Boolean? = null
+        private var _warningLimitOfUnionItems: Int? = null
+        private var _warningLimitOfIntersectItems: Int? = null
+
+        override val translateUnderscoreToSpace: Boolean get() {
+            if (data.metadata.query.translateUnderscoreToSpace != _translateUnderscoreToSpace) {
+                _translateUnderscoreToSpace = data.metadata.query.translateUnderscoreToSpace
+                if(executePlanCache.isNotEmpty()) executePlanCache.clear()
+            }
+            return _translateUnderscoreToSpace!!
+        }
+        override val chineseSymbolReflect: Boolean get() {
+            if (data.metadata.query.chineseSymbolReflect != _chineseSymbolReflect) {
+                _chineseSymbolReflect = data.metadata.query.chineseSymbolReflect
+                if(executePlanCache.isNotEmpty()) executePlanCache.clear()
+            }
+            return _chineseSymbolReflect!!
+        }
+        override val warningLimitOfUnionItems: Int get() {
+            if (data.metadata.query.warningLimitOfUnionItems != _warningLimitOfUnionItems) {
+                _warningLimitOfUnionItems = data.metadata.query.warningLimitOfUnionItems
+                if(executePlanCache.isNotEmpty()) executePlanCache.clear()
+            }
+            return _warningLimitOfUnionItems!!
+        }
+        override val warningLimitOfIntersectItems: Int get() {
+            if (data.metadata.query.warningLimitOfIntersectItems != _warningLimitOfIntersectItems) {
+                _warningLimitOfIntersectItems = data.metadata.query.warningLimitOfIntersectItems
+                if(executePlanCache.isNotEmpty()) executePlanCache.clear()
+            }
+            return _warningLimitOfIntersectItems!!
+        }
     }
 }
--- server/src/main/kotlin/com/heerkirov/hedge/server/library/compiler/utils/AnalysisResult.kt
+++ server/src/main/kotlin/com/heerkirov/hedge/server/library/compiler/utils/AnalysisResult.kt
@@ -1,6 +1,6 @@
 package com.heerkirov.hedge.server.library.compiler.utils
 
-data class AnalysisResult<T, E>(val result: T?, val warnings: List<E> = emptyList(), val errors: List<E> = emptyList()) {
+data class AnalysisResult<T, out E>(val result: T?, val warnings: List<E> = emptyList(), val errors: List<E> = emptyList()) {
     override fun toString(): String {
         return "{\nresult=$result\nwarnings=$warnings\nerrors=$errors\n}"
     }
