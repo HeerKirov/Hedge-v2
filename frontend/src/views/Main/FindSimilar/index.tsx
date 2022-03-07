8140cd693759a371dc5387c4a3c7ffcef6fb14b2
Heer Kirov <HeerKirov@outlook.com>
2022-03-07 16:21:12 +0800

    fix(frontend/find-similar): 修复代码调整的遗留问题

--- frontend/src/views/Main/FindSimilar/index.tsx
+++ frontend/src/views/Main/FindSimilar/index.tsx
@@ -3,9 +3,12 @@ import TopBarLayout from "@/components/layouts/TopBarLayout"
 import TopBarContent from "./TopBarContent"
 import ListView from "./ListView"
 import DetailDialog from "./DetailDialog"
+import { installFindSimilarContext } from "./inject"
 
 export default defineComponent({
     setup() {
+        installFindSimilarContext()
+
         return () => <div>
             <TopBarLayout v-slots={{
                 topBar: () => <TopBarContent/>,
